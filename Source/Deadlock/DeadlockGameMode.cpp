47fab62d0866c453948a1012033b6ecc1dac110e
wonjun16 <jun9816@naver.com>
2024-10-08 17:54:36 +0900

    Init

---
+++ Source/Deadlock/DeadlockGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DeadlockGameMode.h"
+#include "DeadlockCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ADeadlockGameMode::ADeadlockGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/Deadlock/DeadlockGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DeadlockGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADeadlockGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADeadlockGameMode();
+};
+
+
+
