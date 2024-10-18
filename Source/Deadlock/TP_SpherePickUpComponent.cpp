ae4dfbbefa5e36c925f4658baf3e7fc1482f049a
madwil730 <madwil@naver.com>
2024-10-18 13:51:47 +0900

    테스트 코드 커밋

--- Source/Deadlock/TP_SpherePickUpComponent.cpp
+++ Source/Deadlock/TP_SpherePickUpComponent.cpp
@@ -13,6 +13,7 @@ void UTP_SpherePickUpComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
+	UE_LOG(LogTemp, Log, TEXT("Log Message"));
 	// Register our Overlap Event
 	OnComponentBeginOverlap.AddDynamic(this, &UTP_SpherePickUpComponent::OnSphereBeginOverlap);
 
@@ -20,12 +21,15 @@ void UTP_SpherePickUpComponent::BeginPlay()
 
 void UTP_SpherePickUpComponent::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	UE_LOG(LogTemp, Log, TEXT("Overlapped"));
 	// Checking if it is a First Person Character overlapping
 	ADeadlockCharacter* Character = Cast<ADeadlockCharacter>(OtherActor);
 	if (Character != nullptr)
 	{
+		UE_LOG(LogTemp, Log, TEXT("ADeadlockCharacter"));
 		// Notify that the actor is being picked up
-		OnPickUp.Broadcast(Character);
+		//OnPickUp.Broadcast(Character);
+		AttachToComponent(Character->GetMesh(), FAttachmentTransformRules::SnapToTargetNotIncludingScale, FName("weapon"));
 
 		// Unregister from the Overlap Event so it is no longer triggered
 		OnComponentBeginOverlap.RemoveAll(this);
