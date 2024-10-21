3c4f16ac83afa720c0535fdd747de9e7283b4d75
wonjun16 <jun9816@naver.com>
2024-10-21 17:52:59 +0900

    [refactor] clean weaponbase

--- Source/Deadlock/Weapon/WeaponBase.cpp
+++ Source/Deadlock/Weapon/WeaponBase.cpp
@@ -8,16 +8,17 @@
 #include "Kismet/KismetMathLibrary.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Engine/World.h"
+#include "GameFramework/Character.h"
 
 
 // Sets default values
 AWeaponBase::AWeaponBase()
 {
-	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>("Weapon");
-	WeaponMesh->SetCollisionProfileName("Weapon");
-	WeaponMesh->SetSimulatePhysics(true);
-	SetRootComponent(WeaponMesh);
-	bReplicates = true; // Actor가 복제되도록 설정
+	bReplicates = true;
+
+	MaxAmmo = 30;
+	CurAmmo = 0;
+	MyCharacter = nullptr;
 }
 
 // Called when the game starts or when spawned
@@ -32,28 +33,8 @@ void AWeaponBase::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
-void AWeaponBase::CalcStartForwadVector(FVector& StartVec, FVector& EndVec, FVector MuzzleLoc)
-{
-	APlayerCameraManager* CameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
-
-	FVector ActorLocation = CameraManager->GetCameraLocation();
-	FVector ActorForwardVector = CameraManager->GetActorForwardVector();
-
-	float Distance = UKismetMathLibrary::Vector_Distance(MuzzleLoc, ActorLocation);
-
-
-	FVector MultiplyVector = ActorForwardVector * Distance;
-	FVector PlusVector = ActorLocation + MultiplyVector;
-	FVector Multiply5000Vector = ActorForwardVector * 5000;
-	FVector PlusVector2 = ActorLocation + Multiply5000Vector;
-
-	StartVec = PlusVector;
-	MuzzleLoc = PlusVector2;
-}
-
 void AWeaponBase::UseAmmo()
 {
-	Ammo = Ammo - 1;
 }
 
 void AWeaponBase::BindAmmo()
@@ -97,80 +78,34 @@ void AWeaponBase::SpawnProjectile()
 	}
 }
 
-void AWeaponBase::Fire()
+void AWeaponBase::EventReloadTrigger_Implementation(bool bPress)
 {
-	if (HasAuthority()) // 서버일 때
-	{
-		ExecuteFire();
-	}
-	else // 클라이언트일 때 서버에 요청
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "124124123");
-		ServerFire();
-	}
 
 }
 
-// 서버에서 발사 요청
-void AWeaponBase::ServerFire_Implementation()
+void AWeaponBase::EventReload_Implementation()
 {
-	GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "Fire");
-	ExecuteFire(); // 서버에서 실제 발사
+
 }
 
-// 실제 발사 로직
-void AWeaponBase::ExecuteFire()
+void AWeaponBase::EventAttackTrigger_Implementation(bool bPress)
 {
-	if (ProjectileClass)
-	{
-		// 총알 발사 로직
-		FVector SpawnLocation = GetActorLocation(); // 발사 위치
-		FRotator SpawnRotation = GetActorRotation(); // 발사 방향
-		GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnLocation, SpawnRotation);
-	}
+
 }
 
-
-bool AWeaponBase::EventReloadTrigger_Implementation(bool bPress)
+void AWeaponBase::EventAttack_Implementation()
 {
-	// 리로드 트리거 로직 작성
-	if (bPress)
-	{
-		UE_LOG(LogTemp, Log, TEXT("Reload Trigger Pressed"));
-		// 리로드 로직 수행
-		return true;  // 리로드 성공 시 true 반환
-	}
-	else
-	{
-		UE_LOG(LogTemp, Log, TEXT("Reload Trigger Released"));
-		// 리로드 중단 등의 로직 수행
-		return false;
-	}
+
 }
 
-bool AWeaponBase::EventReload_Implementation()
+void AWeaponBase::EventSwitchWeaponTrigger_Implementation(bool bPress)
 {
-	return false;
+
 }
 
-bool AWeaponBase::EventAttackTrigger_Implementation(bool bPress)
+void AWeaponBase::EventSwitchWeapon_Implementation()
 {
-	return false;
-}
 
-bool AWeaponBase::EventAttack_Implementation()
-{
-	return false;
-}
-
-bool AWeaponBase::EventSwitchWeaponTrigger_Implementation(bool bPress)
-{
-	return false;
-}
-
-bool AWeaponBase::EventSwitchWeapon_Implementation()
-{
-	return false;
 }
 
 bool AWeaponBase::IsCanAttack_Implementation()
@@ -188,15 +123,15 @@ bool AWeaponBase::IsCanSwitchWeapon_Implementation()
 	return false;
 }
 
-bool AWeaponBase::EventDrop_Implementation(ACharacter* Character)
+void AWeaponBase::EventDrop_Implementation(ACharacter* Character)
 {
-	return false;
+
 }
 
 EWeaponType AWeaponBase::EventGrabWeapon_Implementation(ACharacter* Character)
 {
 
-	GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "ServerGrab_Implementation");
+	GEngine->AddOnScreenDebugMessage(-1, 15.f, FColor::Green, "Weapon Grab Implementation");
 
 	MyCharacter = Character;
 	MyCharacter->bUseControllerRotationYaw = true;
--- Source/Deadlock/Weapon/WeaponBase.h
+++ Source/Deadlock/Weapon/WeaponBase.h
@@ -5,10 +5,11 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "GameFramework/PlayerController.h"
-#include "GameFramework/Character.h"
 #include "../Interface/WeaponInterface.h"
 #include "../Data/Enums.h"
 #include "WeaponBase.generated.h"
+
+class ACharacter;
 
 UCLASS()
 class DEADLOCK_API AWeaponBase : public AActor, public IWeaponInterface
@@ -19,24 +20,23 @@ public:
 	// Sets default values for this actor's properties
 	AWeaponBase();
 
-UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* WeaponMesh;
 
-UPROPERTY(VisibleAnywhere)
-	int32 Ammo;
+	UPROPERTY(VisibleAnywhere)
+	uint8 MaxAmmo;
 
-UPROPERTY(VisibleAnywhere)
+	UPROPERTY(VisibleAnywhere)
+	uint8 CurAmmo;
+
+	UPROPERTY(VisibleAnywhere)
 	ACharacter* MyCharacter;
 
-UPROPERTY(VisibleAnywhere)
-	EWeaponType WeaponData;
+	UPROPERTY(VisibleAnywhere)
+	EWeaponType WeaponType;
 
-UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Weapon")
-	TSubclassOf<AActor> ProjectileClass; // 발사할 총알 클래스
-
-	// 총 소리 사운드
-UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Weapon")
-	USoundBase* FireSound; // 총 소리 사운드
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = "Weapon")
+	TSubclassOf<AActor> ProjectileClass;
 
 protected:
 	// Called when the game starts or when spawned
@@ -47,7 +47,6 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 public:
-	void CalcStartForwadVector(FVector& StartVec, FVector& EndVec, FVector MuzzleLoc);
 	void UseAmmo();
 	void BindAmmo();
 	void UnBindAmmo();
@@ -56,47 +55,25 @@ public:
 	void GetShootDelayByRPM(float& DeltaTime);
 	void SpawnProjectile();
 
-	UFUNCTION(BlueprintCallable, Category = "Weapon")
-	void Fire();
-	// 서버에서 발사하는 메서드
-	UFUNCTION(Server, Reliable)
-	void ServerFire();
-	// 실제 발사 로직
-	void ExecuteFire();
+	virtual void EventReloadTrigger_Implementation(bool bPress) override;
 
+	virtual void EventReload_Implementation() override;
 
-	virtual bool EventReloadTrigger_Implementation(bool bPress) override;
+	virtual void EventAttackTrigger_Implementation(bool bPress) override;
 
-	// EventReload function implementation
-	virtual bool EventReload_Implementation() override;
+	virtual void EventAttack_Implementation() override;
 
-	// EventAttackTrigger function implementation
-	virtual bool EventAttackTrigger_Implementation(bool bPress) override;
+	virtual void EventSwitchWeaponTrigger_Implementation(bool bPress) override;
 
-	// EventAttack function implementation
-	virtual bool EventAttack_Implementation() override;
+	virtual void EventSwitchWeapon_Implementation() override;
 
-	// EventSwitchWeaponTrigger function implementation
-	virtual bool EventSwitchWeaponTrigger_Implementation(bool bPress) override;
-
-	// EventSwitchWeapon function implementation
-	virtual bool EventSwitchWeapon_Implementation() override;
-
-	// IsCanAttack function implementation
 	virtual bool IsCanAttack_Implementation() override;
 
-	// IsCanReload function implementation
 	virtual bool IsCanReload_Implementation() override;
 
-	// IsCanSwitchWeapon function implementation
 	virtual bool IsCanSwitchWeapon_Implementation() override;
 
-	// EventDrop function implementation
-	virtual bool EventDrop_Implementation(ACharacter* Character) override;
+	virtual void EventDrop_Implementation(ACharacter* Character) override;
 
-	// EventGrabWeapon function implementation
 	virtual EWeaponType EventGrabWeapon_Implementation(ACharacter* Character) override;
-
-
-
 };
