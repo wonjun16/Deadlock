f65d6fbd06cf620f5ebf7534be20d7c1d66eb413
madwil730 <madwil@naver.com>
2024-10-23 17:48:25 +0900

    feat :  마그넷 필드 블루프린트 개발 완료 및 c++코드로 변환

---
+++ Source/Deadlock/GameSystem/MagneticField.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MagneticField.h"
+
+// Sets default values
+AMagneticField::AMagneticField()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	magneticMesh = CreateDefaultSubobject<UStaticMeshComponent>("Weapon");
+	SetRootComponent(magneticMesh);
+	bReplicates = true; // Actor가 복제되도록 설정
+
+}
+
+// Called when the game starts or when spawned
+void AMagneticField::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UKismetSystemLibrary::K2_SetTimer(this, "SetHp", 2.0f, true);
+	//FVector randomVec = 
+	SetActorLocation()
+	
+}
+
+// Called every frame
+void AMagneticField::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AMagneticField::SetHp()
+{
+}
+
+void AMagneticField::CheckRemainTime()
+{
+}
+
---
+++ Source/Deadlock/GameSystem/MagneticField.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MagneticField.generated.h"
+
+UCLASS()
+class DEADLOCK_API AMagneticField : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMagneticField();
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* WeaponMesh;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool TakeDamage;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float RemainTime;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void SetHp();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void CheckRemainTime();
+
+};
