36022ed4e12ce43fba37d2fdaad4762994d7dc29
madwil730 <madwil@naver.com>
2024-10-24 09:52:59 +0900

    feat : ui 파일 커밋

---
+++ Source/Deadlock/UI/MagneticUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MagneticUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEADLOCK_API UMagneticUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
