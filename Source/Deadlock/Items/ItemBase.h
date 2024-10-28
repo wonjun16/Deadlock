c5cd8f2182271c9cc640f932d2350d05d12e68b7
JangSungJoon <amaten98@gmail.com>
2024-10-28 17:52:03 +0900

    [feat] add itembase

---
+++ Source/Deadlock/Items/ItemBase.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemBase.generated.h"
+
+UCLASS()
+class DEADLOCK_API AItemBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItemBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	int MaxCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	int CurrentCount;
+
+	void UseItem(int currentcount);
+
+	void GetItem(int maxcount, int currentcount);
+
+};
