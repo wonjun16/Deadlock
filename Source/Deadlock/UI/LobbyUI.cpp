add21f3c8d7be1bb1fc26b0498c1cd84d868e4af
tlforlrnr <tlforlrnr@gmail.com>
2024-11-04 21:28:20 +0900

    feat : 로비 개발 중간과정 커밋

--- Source/Deadlock/UI/LobbyUI.cpp
+++ Source/Deadlock/UI/LobbyUI.cpp
@@ -8,65 +8,65 @@
 #include "Components/Button.h"
 #include "Kismet/GameplayStatics.h"
 
-//void ULobbyUI::NativeConstruct()
-//{
-//	Super::NativeConstruct(); 
-//
-//	LeftTime = Cast<UTextBlock>(GetWidgetFromName(TEXT("LeftTime")));
-//	AliveCount = Cast<UTextBlock>(GetWidgetFromName(TEXT("AliveCount")));
-//	ChatScroll = Cast<UScrollBox>(GetWidgetFromName(TEXT("ChatScroll")));
-//	ChatBox = Cast<UEditableTextBox>(GetWidgetFromName(TEXT("ChatBox")));
-//	ReadyButton = Cast<UButton>(GetWidgetFromName(TEXT("ReadyButton")));
-//	StartButton = Cast<UButton>(GetWidgetFromName(TEXT("StartButton")));
-//
-//	if (ChatBox)
-//	{
-//		ChatBox->OnTextCommitted.AddDynamic(this, &ULobbyUI::OnCommittedText);
-//	}
-//	if (ReadyButton)
-//	{
-//		ReadyButton->OnClicked.AddDynamic(this, &ULobbyUI::ProcessClicked);
-//	}
-//	if (StartButton)
-//	{
-//		StartButton->OnClicked.AddDynamic(this, &ULobbyUI::ProcessStartButtonClicked);
-//	}
-//
-//	//ATPSGameStateBase* GS = Cast<ATPSGameStateBase>(UGameplayStatics::GetGameState(GetWorld()));
-//	/*if (IsValid(GS))
-//	{
-//		GS->OnChangePlayerCount.AddDynamic(this, &ULobbyUI::SetAliveCount);
-//	}*/
-//}
-//
-//void ULobbyUI::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
-//{
-//}
-//
-//void ULobbyUI::ProcessStartButtonClicked()
-//{
-//}
-//
-//void ULobbyUI::ProcessClicked()
-//{
-//}
-//
-//void ULobbyUI::OnCommittedText(const FText& Text, ETextCommit::Type CommitMethod)
-//{
-//}
-//
-//void ULobbyUI::SetLeftTime(FString NewLeftTime)
-//{
-//}
-//
-//void ULobbyUI::SetAliveCount(uint32 NewAliveCount)
-//{
-//}
-//
-//void ULobbyUI::AddChatMessage(FText AddMessage)
-//{
-//}
-//
-//void ULobbyUI::ShowStartrButton(bool Server)
-//{
-//}
+void ULobbyUI::NativeConstruct()
+{
+	Super::NativeConstruct(); 
+
+	LeftTime = Cast<UTextBlock>(GetWidgetFromName(TEXT("LeftTime")));
+	AliveCount = Cast<UTextBlock>(GetWidgetFromName(TEXT("AliveCount")));
+	ChatScroll = Cast<UScrollBox>(GetWidgetFromName(TEXT("ChatScroll")));
+	ChatBox = Cast<UEditableTextBox>(GetWidgetFromName(TEXT("ChatBox")));
+	ReadyButton = Cast<UButton>(GetWidgetFromName(TEXT("ReadyButton")));
+	StartButton = Cast<UButton>(GetWidgetFromName(TEXT("StartButton")));
+
+	//if (ChatBox)
+	//{
+	//	ChatBox->OnTextCommitted.AddDynamic(this, &ULobbyUI::OnCommittedText);
+	//}
+	//if (ReadyButton)
+	//{
+	//	ReadyButton->OnClicked.AddDynamic(this, &ULobbyUI::ProcessClicked);
+	//}
+	//if (StartButton)
+	//{
+	//	StartButton->OnClicked.AddDynamic(this, &ULobbyUI::ProcessStartButtonClicked);
+	//}
+
+	//ATPSGameStateBase* GS = Cast<ATPSGameStateBase>(UGameplayStatics::GetGameState(GetWorld()));
+	/*if (IsValid(GS))
+	{
+		GS->OnChangePlayerCount.AddDynamic(this, &ULobbyUI::SetAliveCount);
+	}*/
+}
+
+void ULobbyUI::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+}
+
+void ULobbyUI::ProcessStartButtonClicked()
+{
+}
+
+void ULobbyUI::ProcessClicked()
+{
+}
+
+void ULobbyUI::OnCommittedText(const FText& Text, ETextCommit::Type CommitMethod)
+{
+}
+
+void ULobbyUI::SetLeftTime(FString NewLeftTime)
+{
+}
+
+void ULobbyUI::SetAliveCount(uint32 NewAliveCount)
+{
+}
+
+void ULobbyUI::AddChatMessage(FText AddMessage)
+{
+}
+
+void ULobbyUI::ShowStartrButton(bool Server)
+{
+}
--- Source/Deadlock/UI/LobbyUI.h
+++ Source/Deadlock/UI/LobbyUI.h
@@ -19,46 +19,46 @@ class DEADLOCK_API ULobbyUI : public UUserWidget
 {
 	GENERATED_BODY()
 
-//public:
-//	virtual void NativeConstruct() override;
-//
-//	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
-//
-//
-//	UFUNCTION()
-//	void ProcessStartButtonClicked();
-//
-//	UFUNCTION()
-//	void ProcessClicked();
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UTextBlock* LeftTime;
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UTextBlock* AliveCount;
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UScrollBox* ChatScroll;
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UEditableTextBox* ChatBox;
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UButton* ReadyButton;
-//
-//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
-//	UButton* StartButton;
-//
-//	UFUNCTION()
-//	void OnCommittedText(const FText& Text, ETextCommit::Type CommitMethod);
-//
-//	void SetLeftTime(FString NewLeftTime);
-//
-//	UFUNCTION()
-//	void SetAliveCount(uint32 NewAliveCount);
-//
-//	void AddChatMessage(FText AddMessage);
-//
-//	void ShowStartrButton(bool Server);
+public:
+	virtual void NativeConstruct() override;
+
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UTextBlock* LeftTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UTextBlock* AliveCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UScrollBox* ChatScroll;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UEditableTextBox* ChatBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UButton* ReadyButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+	UButton* StartButton;
+
+
+	UFUNCTION()
+	void ProcessStartButtonClicked();
+
+	UFUNCTION()
+	void ProcessClicked();
+
+	UFUNCTION()
+	void OnCommittedText(const FText& Text, ETextCommit::Type CommitMethod);
+
+	void SetLeftTime(FString NewLeftTime);
+
+	UFUNCTION()
+	void SetAliveCount(uint32 NewAliveCount);
+
+	void AddChatMessage(FText AddMessage);
+
+	void ShowStartrButton(bool Server);
 	
 };
