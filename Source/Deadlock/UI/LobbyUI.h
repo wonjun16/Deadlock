1d37920872411d1ee9a7f0cf2a44f514d5243fd7
madwil730 <madwil@naver.com>
2024-11-04 17:37:20 +0900

    로비 개발 커밋

--- Source/Deadlock/UI/LobbyUI.h
+++ Source/Deadlock/UI/LobbyUI.h
@@ -6,6 +6,11 @@
 #include "Blueprint/UserWidget.h"
 #include "LobbyUI.generated.h"
 
+class UScrollBox;
+class UEditableTextBox;
+class UTextBlock;
+class UButton;
+
 /**
  * 
  */
@@ -13,5 +18,47 @@ UCLASS()
 class DEADLOCK_API ULobbyUI : public UUserWidget
 {
 	GENERATED_BODY()
+
+//public:
+//	virtual void NativeConstruct() override;
+//
+//	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+//
+//
+//	UFUNCTION()
+//	void ProcessStartButtonClicked();
+//
+//	UFUNCTION()
+//	void ProcessClicked();
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UTextBlock* LeftTime;
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UTextBlock* AliveCount;
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UScrollBox* ChatScroll;
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UEditableTextBox* ChatBox;
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UButton* ReadyButton;
+//
+//	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
+//	UButton* StartButton;
+//
+//	UFUNCTION()
+//	void OnCommittedText(const FText& Text, ETextCommit::Type CommitMethod);
+//
+//	void SetLeftTime(FString NewLeftTime);
+//
+//	UFUNCTION()
+//	void SetAliveCount(uint32 NewAliveCount);
+//
+//	void AddChatMessage(FText AddMessage);
+//
+//	void ShowStartrButton(bool Server);
 	
 };
